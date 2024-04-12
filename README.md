a7adec3dd69ac74e3fc9f8434b6941b619b33bec
Poko-Apps <parimals741@gmail.com>
2024-04-13 00:23:05 +0530

    Curl-8.7.1 OpenSSl-3.3.0

---
+++ README.md
@@ -0,0 +1,19 @@
+# Curl & OpenSSL Builds for Android
+This GitHub Action compiles the latest OpenSSL source and the latest Curl source as a static library for Android native (C/C++) development.
+
+## ABIs We Build (Currently)
+* [x] armeabi-v7a
+* [x] arm64-v8a
+* [ ] x86
+* [ ] x86_64
+
+## Notes
+* Curl is built with SSL/TLS support.
+* Curl builds do not include zlib support.
+
+## Download
+Downloads are available in the release section. Release names and tags follow this format: {curl_version-openssl_version}.
+
+## Thanks
+* [217heidai](https://github.com/217heidai/openssl_for_android)
+* [liaokun](https://zhuanlan.zhihu.com/p/614298413?utm_id=0)
---
+++ build_all.sh
@@ -0,0 +1,85 @@
+#!/bin/bash -e
+
+# change our dir to where our script is, and then print pwd
+WORK_PATH=$(cd "$(dirname "$0")";pwd)
+MIN_API=21
+HOST_TAG=linux-x86_64
+BUILD_DIR=${WORK_PATH}/build
+OPENSSL_SRC_DIR=${WORK_PATH}/openssl-${OPENSSL_VERSION}
+CURL_SRC_DIR=${WORK_PATH}/curl-${CURL_VERSION}
+
+export ANDROID_NDK_ROOT=${WORK_PATH}/android-ndk-${ANDROID_NDK_VERSION}
+TOOLCHAIN=${ANDROID_NDK_ROOT}/toolchains/llvm/prebuilt/${HOST_TAG}
+PATH=${TOOLCHAIN}/bin:$PATH
+
+
+function build_openssl() {
+    TARGET_HOST=$1
+    OPENSSL_ARCH=$2
+    INSTALL_DIR=${BUILD_DIR}/openssl-${OPENSSL_VERSION}/${ANDROID_ABI}
+    mkdir -p ${INSTALL_DIR}
+    
+    ./Configure ${OPENSSL_ARCH} no-tests no-unit-test no-shared -static no-asm -D__ANDROID_API__=${MIN_API} --prefix=${INSTALL_DIR}
+    make && make install_sw
+    #clean up
+    rm -rf ${OPENSSL_SRC_DIR}
+    rm -rf ${INSTALL_DIR}/bin
+    rm -rf ${INSTALL_DIR}/share
+    rm -rf ${INSTALL_DIR}/ssl
+    rm -rf ${INSTALL_DIR}/lib/engines*
+    rm -rf ${INSTALL_DIR}/lib/pkgconfig
+    rm -rf ${INSTALL_DIR}/lib/ossl-modules
+}
+
+function build_curl() {
+    export TARGET_HOST=$1
+    export ANDROID_ARCH=${ANDROID_ABI}
+    export AR=${TOOLCHAIN}/bin/llvm-ar
+    export CC=${TOOLCHAIN}/bin/${TARGET_HOST}${MIN_API}-clang
+    export AS=${CC}
+    export CXX=${TOOLCHAIN}/bin/${TARGET_HOST}${MIN_API}-clang++
+    export LD=${TOOLCHAIN}/bin/ld
+    export RANLIB=${TOOLCHAIN}/bin/llvm-ranlib
+    export STRIP=${TOOLCHAIN}/bin/llvm-strip
+    
+    INSTALL_DIR=${BUILD_DIR}/curl-${CURL_VERSION}/${ANDROID_ABI}
+    mkdir -p ${INSTALL_DIR}
+    
+    ./configure --host=${TARGET_HOST} \
+                --target=${TARGET_HOST} \
+                --prefix=${INSTALL_DIR} \
+                --with-openssl=${BUILD_DIR}/openssl-${OPENSSL_VERSION}/${ANDROID_ABI} \
+                --with-pic --disable-shared
+
+    make
+    make install
+    #clean up
+    rm -rf ${CURL_SRC_DIR}
+    rm -rf ${INSTALL_DIR}/bin
+    rm -rf ${INSTALL_DIR}/share
+}
+
+
+
+
+if [ "$ANDROID_ABI" == "armeabi-v7a" ]
+then
+    cd ${OPENSSL_SRC_DIR}
+    #build openssl first
+    build_openssl armv7a-linux-androideabi android-arm
+    #if openssl build success, then build curl
+    cd ${CURL_SRC_DIR}
+    build_curl armv7a-linux-androideabi
+    
+elif [ "$ANDROID_ABI" == "arm64-v8a" ]
+then
+    cd ${OPENSSL_SRC_DIR}
+    #build openssl first
+    build_openssl aarch64-linux-android android-arm64
+    #if openssl build success, then build curl
+    cd ${CURL_SRC_DIR}
+    build_curl aarch64-linux-android
+else
+    echo "Unsupported target ABI: $ANDROID_ABI"
+    exit 1
+fi
